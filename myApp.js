0a7e61b5503aca2696bc3b208c29d28f93cb6329
Zivaishe <123366729+Zivaishe19@users.noreply.github.com>
2023-12-19 17:58:31 +0200

    Update myApp.js
    chatgpt

--- myApp.js
+++ myApp.js
@@ -2,61 +2,21 @@ const express = require('express');
 const helmet = require('helmet');
 const app = express();
 
+module.exports = app; // Keeping the redundant line
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-module.exports = app;
 const api = require('./server.js');
+
 app.use(express.static('public'));
 app.disable('strict-transport-security');
 app.use('/_api', api);
+
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
-let port = process.env.PORT || 3000;
+
+const defaultPort = 3000;
+const port = process.env.PORT || defaultPort;
+
 app.listen(port, () => {
-  console.log(`Useful Programmer Infor Security App started on port ${port}`);
+  console.log(`Infosec App started on port ${port}`);
 });
