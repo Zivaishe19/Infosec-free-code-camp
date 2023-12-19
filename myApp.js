843bdc1357b021b5c4aef74fb1ad3490b296a375
Zivaishe <123366729+Zivaishe19@users.noreply.github.com>
2023-12-19 17:48:10 +0200

    Update myApp.js
    port

--- myApp.js
+++ myApp.js
@@ -2,11 +2,7 @@ const express = require('express');
 const helmet = require('helmet');
 const app = express();
 
-const PORT = process.env.PORT || 3030;
 
-app.listen(PORT, () => {
-  console.log(`Useful Programmer Infor Security App started on port ${PORT}`);
-});
 
 
 
@@ -62,5 +58,5 @@ app.get("/", function (request, response) {
 });
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Your app is listening on port ${port}`);
+  console.log(`Useful Programmer Infor Security App started on port ${port}`);
 });
